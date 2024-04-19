e246c87233169b571a2fcf4ab4fa019deb1bc06c
hxym <15330485163@163.com>
2024-04-19 13:52:27 +0800

    气球块编辑方式 不能 和  经典编辑器 混为一谈  否则会出问题

--- src/pages/ckeditor/ckeditorBalloon.tsx
+++ src/pages/ckeditor/ckeditorBalloon.tsx
@@ -1,46 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useEffect, useRef } from 'react';
-import { Editor } from '@ckeditor/ckeditor5-core';
-import BalloonEditorBase from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import React from 'react';
+
 
 
 const CkeditorBalloon: React.FC = () => {
-    const editorContainerRef = useRef<HTMLDivElement>(null);
-    // 使用 CKEditor 5 的 Editor 类型
-    const editorInstanceRef = useRef<Editor | null>(null);
-
-    useEffect(() => {
-        if (editorContainerRef.current) {
-            BalloonEditorBase.create(editorContainerRef.current, {
-                plugins: [Essentials, Paragraph, Bold, Italic],
-                toolbar: ['bold', 'italic']
-            })
-            .then(editor => {
-                editorInstanceRef.current = editor;
-                console.log('Editor was initialized', editor);
-            })
-            .catch(error => {
-                console.error('There was a problem initializing the editor.', error);
-            });
-
-            return () => {
-                if (editorInstanceRef.current) {
-                    editorInstanceRef.current.destroy()
-                        .catch(error => console.error('Error during editor cleanup', error));
-                }
-            };
-        }
-    }, []);
+ 
 
     return (
-        <div className="App">
-            <h2>Using a custom CKEditor 5 build in a functional component</h2>
-            <div ref={editorContainerRef}></div>
-        </div>
+       <div>
+            <h2>demo。。。。。</h2>
+       </div>
     );
 };
 
